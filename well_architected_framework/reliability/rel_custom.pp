e65dc133322b531d809791861055b95cd5cf2d6e
mpcabral <matheus.cabral@tild.com.br>
2025-04-04 16:54:41 -0300

    feat: add new RDS and EKS controls for support and extended support checks

---
+++ well_architected_framework/reliability/rel_custom.pp
@@ -0,0 +1,17 @@
+locals {
+  well_architected_framework_rel09_common_tags = merge(local.well_architected_framework_reliability_common_tags, {
+    question_id = "backing-up-data"
+  })
+}
+
+benchmark "well_architected_framework_rel_custom" {
+  title       = "Ensure Supported Service Versions"
+  description = "Maintaining workloads on supported software versions is critical for reliability and security. This benchmark validates that key AWS services—namely RDS and EKS—operate on versions within defined support schedules. Instances that are approaching end-of-support or running below the minimum thresholds are flagged, enabling proactive upgrades and minimizing operational risk."
+  children = [
+    aws_compliance.control.rds_instance_support_check,
+    aws_compliance.control.rds_db_instance_extended_support,
+    aws_compliance.control.eks_cluster_support_check,
+    aws_compliance.control.eks_cluster_extended_support
+  ]
+  tags = local.well_architected_framework_rel09_common_tags
+}
--- well_architected_framework/reliability/reliability.pp
+++ well_architected_framework/reliability/reliability.pp
@@ -8,6 +8,7 @@ benchmark "well_architected_framework_reliability" {
   title       = "Reliability"
   description = "The reliability pillar focuses on workloads performing their intended functions and how to recover quickly from failure to meet demands. Key topics include distributed system design, recovery planning, and adapting to changing requirements."
   children = [
+    benchmark.well_architected_framework_rel_custom,
     benchmark.well_architected_framework_rel01,
     benchmark.well_architected_framework_rel02,
     benchmark.well_architected_framework_rel06,
